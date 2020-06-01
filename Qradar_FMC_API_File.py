3deb63d83b2e60e65ac15d853f0a61809acf35ac
Rajan Singh <rajan06.singh@gmail.com>
2020-06-01 13:31:04 -0400

    Update Qradar_FMC_API_File.py

--- Qradar_FMC_API_File.py
+++ Qradar_FMC_API_File.py
@@ -40,7 +40,7 @@ except Exception as err:
  
 headers['X-auth-access-token']=auth_token
  
-api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/object/networkgroups/0050568F-3EDA-0ed3-0000-274877911250"    # param
+api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/object/networkgroups/0050568F-3EFA-0ed4-0000-274867911250"    # param
 url = server + api_path
 if (url[-1] == '/'):
     url = url[:-1]
@@ -49,7 +49,7 @@ if (url[-1] == '/'):
  
 
 put_data = {
-  "id": "0050568F-3EDA-0ed3-0000-274877911250",
+  "id": "0050568F-3EFA-0ed4-0000-274867911250",
   "name": "Blacklist-IP-Pool",
   "type": "NetworkGroup",
   "literals": [
